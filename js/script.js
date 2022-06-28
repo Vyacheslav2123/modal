e99f24deb227c666b298d7827a03900222864d5e
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2022-06-28 14:50:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,64 +1,44 @@
 function next1() {
-    var input = document.getElementsByName('home');
-
-    for (var i = 0; i < input.length; i++) {
-        var radio = input[i];
-        if (radio.checked) {
+        let radio = document.querySelector(`input[type="radio"][name="home"]:checked`);
+        if (radio.value !== "") {
             window.scrollTo(0, 850)
         } else {
             document.getElementById( 'draw1' ).innerHTML="Выберите вариант";
-        }
     }
 }
 
 function next2() {
-    var input = document.getElementsByName('time');
-
-    for (var i = 0; i < input.length; i++) {
-        var radio = input[i];
-        if (radio.checked) {
-            window.scrollTo(0, 1700)           
+    let radio = document.querySelector(`input[type="radio"][name="time"]:checked`);
+        if (radio.value !== "") {
+            window.scrollTo(0, 850)
         } else {
-            document.getElementById( 'draw2' ).innerHTML="Выберите вариант";            
-        }
+            document.getElementById( 'draw1' ).innerHTML="Выберите вариант";
     }
 }
 
 function next3() {
-    var input = document.getElementsByName('floor');
-
-    for (var i = 0; i < input.length; i++) {
-        var radio = input[i];
-        if (radio.type == "radio" && radio.checked) {
-            window.scrollTo(0, 2550)
-        } else {
-            document.getElementById( 'draw3' ).innerHTML="Выберите вариант";
-        }
-    }
+    let radio = document.querySelector(`input[type="radio"][name="floor"]:checked`);
+    if (radio.value !== "") {
+        window.scrollTo(0, 850)
+    } else {
+        document.getElementById( 'draw1' ).innerHTML="Выберите вариант";
+}
 }
 
 function next4() {
-    var input = document.getElementsByName('budget');
-
-    for (var i = 0; i < input.length; i++) {
-        var radio = input[i];
-        if (radio.type == "radio" && radio.checked) {
-            window.scrollTo(0, 3400)
-        } else {
-            document.getElementById( 'draw4' ).innerHTML="Выберите вариант";
-        }
-    }
+    let radio = document.querySelector(`input[type="radio"][name="budget"]:checked`);
+    if (radio.value !== "") {
+        window.scrollTo(0, 850)
+    } else {
+        document.getElementById( 'draw1' ).innerHTML="Выберите вариант";
+}
 }
 
 function next5() {
-    var input = document.getElementsByName('square');
-
-    for (var i = 0; i < input.length; i++) {
-        var radio = input[i];
-        if (radio.type == "radio" && radio.checked) {
-            window.scrollTo(0, 4250)
-        } else {
-            document.getElementById( 'draw5' ).innerHTML="Выберите вариант";
-        }
-    }
+    let radio = document.querySelector(`input[type="radio"][name="square"]:checked`);
+    if (radio.value !== "") {
+        window.scrollTo(0, 850)
+    } else {
+        document.getElementById( 'draw1' ).innerHTML="Выберите вариант";
+}
 }
